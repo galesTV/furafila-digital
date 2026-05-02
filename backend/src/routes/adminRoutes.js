098214403fa8e1f35e933a07914c49febd089b60
Gael Guzman <137447978+galesTV@users.noreply.github.com>
2026-05-02 00:00:31 -0300

    feat: implementa login e cadastro de alunos com bcrypt

--- backend/src/routes/adminRoutes.js
+++ backend/src/routes/adminRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const adminController = require('../controllers/adminController');
 
-router.post('/login', adminController.loginAdmin);
 router.post('/register', adminController.registerAdmin);
+router.post('/login', adminController.loginAdmin);
 
 module.exports = router;
